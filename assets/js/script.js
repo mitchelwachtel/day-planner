3a84de71d0f60d6cfdcf1691967185bae62a9c51
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-01-29 07:47:02 -0500

    rows are colored correctly by the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,37 @@
-$(currentDay).text(moment().format('dddd, MMMM Do'));
+$(currentDay).text(moment().format("dddd, MMMM Do"));
 
-var h = moment().format('h');
-var ap = moment().format('a');
+var h = moment().format("h");
+var ampm = moment().format("a");
 
-var timeArr = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-
-$.each(timeArr, function(i) {
-    var rowId = '#' + timeArr[i];
-    if (ap == 'am') {
-        if (h == '9' || h == '10' || h == '11' || h == '12') {
-          if (h == '12') {
-              
-          }
-    }
+var hNum = parseInt(h);
+if (ampm = 'pm') {
+  hNum += 12;
 }
-    if (timeArr[i] == h) {
-        
-        
-        $(rowId).addClass('present');
-    } else if (timeArr[] ){
-        $(rowId).addClass('future');
-    }
-})
+
+// Using times on the 24hr military time for convenience
+var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+// This jQuery iterator looks at each time from 9am to 5pm (17) and compares them to the actual time to determine the color of each row.
+$.each(timeArr, function (i) {
+  var rowId = "#" + timeArr[i];
+  if (timeArr[i] < h) {
+    $(rowId).addClass('past');
+  } else if (timeArr[i] === h) {
+    $(rowId).addClass('present');
+  } else if (timeArr[i] > h) {
+    $(rowId).addClass('future');
+  }
+});
+//     if (timeArr[i] == h) {
+
+//         $(rowId).addClass('present');
+//     } else if (timeArr[] ){
+//         $(rowId).addClass('future');
+//     }
+// })
 
 // if (ap == 'am') {
 //     console.log('HEY!');
 // }
 
 // $(rowId).addClass('future');
-
