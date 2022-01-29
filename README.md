54f9f39d32f63166a3fb0a4fa4fcae95d8b53caa
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-01-29 15:06:39 -0500

    get button click to log content from associated textarea

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # day-planner
+
+
+HELPFUL Resource when trying to access data from button click: https://stackoverflow.com/questions/7723188/what-properties-can-i-use-with-event-target
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 $(currentDay).text(moment().format("dddd, MMMM Do"));
 
+var txt9El = $('#text9');
+var txt10El = $('#text10');
+var txt11El = $('#text11');
+var txt12El = $('#text12');
+var txt13El = $('#text13');
+var txt14El = $('#text14');
+var txt15El = $('#text15');
+var txt16El = $('#text16');
+var txt17El = $('#text17');
+
+var btn9El = $('#btn9');
+var btn10El = $('#btn10');
+var btn11El = $('#btn11');
+var btn12El = $('#btn12');
+var btn13El = $('#btn13');
+var btn14El = $('#btn14');
+var btn15El = $('#btn15');
+var btn16El = $('#btn16');
+var btn17El = $('#btn17');
+
+
 var h = moment().format("h");
 var ampm = moment().format("a");
 
@@ -16,9 +37,20 @@ $.each(timeArr, function (i) {
   var rowId = "#" + timeArr[i];
   if (timeArr[i] < hNum) {
     $(rowId).addClass('past');
-  } else if (timeArr[i] == hNum) {
+  } else if (timeArr[i] === hNum) {
     $(rowId).addClass('present');
   } else if (timeArr[i] > hNum) {
     $(rowId).addClass('future');
   }
 });
+
+$('.btn').on('click', save);
+
+function save(event) {
+    console.log(event.target.attributes.data.value);
+    var x = event.target.attributes.data.value;
+    console.log(x);
+    var k = $('#text'+ x);
+    console.log(k);
+    console.log(k.val());
+}
