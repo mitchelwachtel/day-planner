2fd9aefcd2f5d417507db5472b7d89a727e5b1ee
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-01-30 13:44:26 -0500

    explained js with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var ampm = moment().format("a");
 // When the page opens, it will compare this unique day number with the keys stored. If it finds one, it will populate the textareas.
 var uniqueDay = moment().format("MM")+moment().format("DD")+moment().format("Y");
 
-
 var saveArray = [];
 
+// if converted to an integer, then we can add to it and compare with inequalities
 var hNum = parseInt(h);
 if (ampm = 'pm' && h != 12) {
   hNum += 12;
@@ -32,7 +32,9 @@ $.each(timeArr, function (i) {
 // Selecting all buttons with the class 'btn'
 $('.btn').on('click', save);
 
-
+// Upon clicking save, each button has a data attribute that is being collected and stored for x, this allows us to know which button controls which textarea
+// Once we know what textarea we are pulling from we can grab that text and store it along with the time in an object
+// Push the object to an array, and store the array as a string in local storage
 function save(event) {
     var x = event.target.attributes.data.value;
     var k = $('#text'+ x);
@@ -46,6 +48,10 @@ function save(event) {
     localStorage.setItem(uniqueDay, JSON.stringify(saveArray));
 }
 
+// useStorage is called every time index.html is loaded. It uses the uniqueDay value to see if there is anything stored locally for TODAY!
+// If anything is stored to TODAY, then useStorage gets the string from local and converts it to an Array
+// Finally, useStorage iterates through this array and the most recently stored text from localStorage populates each textarea
+// Older saves on the same day are essentially overwritten my more recent ones as useStorage iterates through the array, but the older saves do EXIST in the array.
 function useStorage () {
 
     if (localStorage.getItem(uniqueDay) !== null) {
