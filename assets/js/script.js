dce422bb183989606e90a61fabda52adacbffd8f
Mitchel Wachtel <mitchel.wachtel@gmail.com>
2022-01-30 13:25:36 -0500

    storage is used to populate text fields upon load of HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,19 @@
 $(currentDay).text(moment().format("dddd, MMMM Do"));
 
-var txt9El = $('#text9');
-var txt10El = $('#text10');
-var txt11El = $('#text11');
-var txt12El = $('#text12');
-var txt13El = $('#text13');
-var txt14El = $('#text14');
-var txt15El = $('#text15');
-var txt16El = $('#text16');
-var txt17El = $('#text17');
-
-
 var h = moment().format("h");
 var ampm = moment().format("a");
 // Created a var that stores a unique number for each day. This is the key on which I can store an array with all of the information SAVED for that day.
 // When the page opens, it will compare this unique day number with the keys stored. If it finds one, it will populate the textareas.
 var uniqueDay = moment().format("MM")+moment().format("DD")+moment().format("Y");
-console.log(uniqueDay);
+
 
 var saveArray = [];
 
 var hNum = parseInt(h);
-if (ampm = 'pm') {
+if (ampm = 'pm' && h != 12) {
   hNum += 12;
 }
-
+console.log(hNum);
 // Using times on the 24hr military time for convenience
 var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
@@ -53,5 +42,22 @@ function save(event) {
     
     saveArray.push(object);
     console.log(saveArray);
-    localStorage.setItem(uniqueDay, saveArray);
+    localStorage.setItem(uniqueDay, JSON.stringify(saveArray));
+}
+
+function useStorage () {
+    // var keys = Object.keys(localStorage);
+
+    if (localStorage.getItem(uniqueDay) !== undefined) {
+        var y = localStorage.getItem(uniqueDay);
+        saveArray = JSON.parse(y);
+    }
+    console.log(saveArray);
+
+    for (i=0; i<saveArray.length; i++) {
+        var x = saveArray[i].time;
+        var id = $('#text'+ x);
+        id.text(saveArray[i].text);
+
+    }
 }
